1788ad47c4c5acf4a7fc3694d1113914eb7d9625
unknown <gdobson91@gmail.com>
2019-06-25 20:40:27 +1000

    Updates

---
+++ precache-manifest.d0b4cf503f1152908358739aba06a68f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e45da104e8b06aff5b53e101b586dd",
+    "url": "/group-bet/index.html"
+  },
+  {
+    "revision": "81e556fe3b5303211ad1",
+    "url": "/group-bet/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "002eafd9495227ea25c6",
+    "url": "/group-bet/static/css/main.621e3f9b.chunk.css"
+  },
+  {
+    "revision": "81e556fe3b5303211ad1",
+    "url": "/group-bet/static/js/2.0ef9ac0a.chunk.js"
+  },
+  {
+    "revision": "002eafd9495227ea25c6",
+    "url": "/group-bet/static/js/main.684b6220.chunk.js"
+  },
+  {
+    "revision": "d2413a9b496bb69efd0c",
+    "url": "/group-bet/static/js/runtime~main.c5729a50.js"
+  }
+]);
